e98b061df9a2199709123f46a59da6e8cb225082
Yamato605 <ymt-956@outlook.jp>
2025-05-21 12:45:03 +0900

    Update!
    新しいコードを追加しました。

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, GitHub");
+
     String greeting  = "Hello, ";
     String name = "Hamada";
     System.out.println(greeting + name);
